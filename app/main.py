e9f58c29db23d15b78fe30892122b4840a44474c
inada-s <mecha.is06@gmail.com>
2020-07-18 14:42:43 +0000

    join request

--- app/main.py
+++ app/main.py
@@ -7,13 +7,15 @@ def main():
     player_key = sys.argv[2]
     print('ServerUrl: %s; PlayerKey: %s' % (server_url, player_key))
 
-    res = requests.post(server_url, data=player_key)
+    print("send JOIN")
+    res = requests.post(server_url, data=[2, player_key, []])
     if res.status_code != 200:
         print('Unexpected server response:')
         print('HTTP code:', res.status_code)
         print('Response body:', res.text)
         exit(2)
     print('Server response:', res.text)
+
 
 
 if __name__ == '__main__':
