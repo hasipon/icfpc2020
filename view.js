e2ff26c86c71193cc28ff0a27e142dbdc0776ab9
Tetsuya Shiota <tetsuya.shiota.1231@gmail.com>
2020-07-18 14:41:24 +0900

    add visualizer

---
+++ view.js
@@ -0,0 +1,29 @@
+const canvas = document.getElementById('canvas');
+
+const ctx = canvas.getContext('2d');
+
+function draw(){
+    const text = document.getElementById('input').value;
+    const textArray = text.split(/\r\n|\r|\n/);
+    textArray.forEach(
+        line => {
+            const regexp = /\((-?[0-9]+), *(-?[0-9]+)\)/g;
+           const poss = line.match(regexp);
+           poss.forEach(
+               pos => {
+                   const regexp2 = /\((-?[0-9]+), *(-?[0-9]+)\)/;
+                   pos = pos.match(regexp2)
+                   console.log(pos);
+                   plot(pos[1], pos[2]);
+               }
+           )
+           console.log(poss);
+        }
+    )
+}
+
+function plot(x, y) {
+    console.log(x);
+    console.log(y);
+    ctx.fillRect(500+x*100,500+y*100,100,100);
+}
