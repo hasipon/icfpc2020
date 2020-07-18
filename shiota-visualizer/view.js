95227bb7841fb958a4ca390d1ded8df4f327602a
Tetsuya Shiota <tetsuya.shiota.1231@gmail.com>
2020-07-18 18:00:02 +0900

    update visualizer

--- shiota-visualizer/view.js
+++ shiota-visualizer/view.js
@@ -2,8 +2,20 @@ const canvas = document.getElementById('canvas');
 
 const ctx = canvas.getContext('2d');
 
+
+function clearCanvas(){
+    console.log("clear");
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+
 function draw(){
+    const scale = document.getElementById('scale').value;
 	const lines = document.getElementById('input').value;
+	const element = document.getElementById( "color" ) ;
+	const color = element.color.value;
+	ctx.fillStyle = color;
+
 	const line = lines.split(/\r\n|\r|\n/).join();
 	const regexp = /[\(\[](-?[0-9]+), *(-?[0-9]+)[\)\]]/g;
 	const poss = line.match(regexp);
@@ -11,16 +23,18 @@ function draw(){
 		pos => {
 			const regexp2 = /[\(\[](-?[0-9]+), *(-?[0-9]+)[\)\]]/;
 			pos = pos.match(regexp2)
-			console.log(pos);
-			plot(pos[1], pos[2]);
+			plot(pos[1], pos[2], scale, color);
 		}
 	);
-	console.log(poss);
 }
 
-function plot(x, y) {
-	console.log(x);
-	console.log(y);
-	const scale = 10;
-	ctx.fillRect(500+x*scale,500+y*scale,scale,scale);
+const base = 500;
+
+function plot(x, y, scale, color) {
+	ctx.fillRect(conv(x, scale), conv(y, scale), scale, scale);
 }
+
+function conv(x, scale){
+	return base+ x*scale;
+}
+
