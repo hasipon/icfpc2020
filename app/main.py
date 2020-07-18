deb2f6782920328c0dd730c00f6a5ce48f3bf053
hasipon <hasipon@users.noreply.github.com>
2020-07-18 14:50:50 +0000

    join

--- app/main.py
+++ app/main.py
@@ -1,14 +1,50 @@
 import requests
 import sys
+from typing import *
+
+
+def modulate(o: Any) -> str:
+    if isinstance(o, int):
+        n = int(o)
+        if n == 0:
+            return "010"
+
+        positive = 0 <= n
+        n = abs(n)
+        ans = ""
+
+        if positive:
+            ans += "01"
+        else:
+            ans += "10"
+
+        width = 1 + (n.bit_length()-1)//4
+        ans += "1" * width + "0"
+        nstr = "{:b}".format(n)
+        ans += "0" * (width * 4 - len(nstr)) + nstr
+        return ans
+
+    elif isinstance(o, Iterable):
+        ans = ""
+        for e in o:
+            ans += "11" + modulate(e)
+        ans += "00"
+        return ans
+
+    else:
+        raise ValueError("unsupported object type")
 
 
 def main():
     server_url = sys.argv[1]
-    player_key = sys.argv[2]
+    player_key = int(sys.argv[2])
     print('ServerUrl: %s; PlayerKey: %s' % (server_url, player_key))
 
     print("send JOIN")
-    res = requests.post(server_url, data=[2, player_key, []])
+    join_request = modulate([2, player_key])
+    print(f"join_request = {repr(join_request)}")
+
+    res = requests.post(f'{server_url}/aliens/send', data=join_request)
     if res.status_code != 200:
         print('Unexpected server response:')
         print('HTTP code:', res.status_code)
