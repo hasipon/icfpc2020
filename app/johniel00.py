874acb7752be0f8e0a25e9e1f708de1f9adf3bb6
Johniel <johniel.s.m@gmail.com>
2020-07-20 07:21:39 +0900

    update 00

--- app/johniel00.py
+++ app/johniel00.py
@@ -3,10 +3,10 @@ from functools import reduce
 from lib import modulate, demodulate_v2, conv, conv_cons, calc_orbit, calc_life, calc_plan
 
 class Ship:
-    def __init__(role, shipId, pos, v, param, x5, x6, x7):
+    def __init__(self, role, shipId, pos, v, param, x5, x6, x7):
         self.role = role
         self.shipId = shipId
-        self.pos = p
+        self.pos = pos
         self.v = v
         self.param = param
         self.x5 = x5
@@ -27,47 +27,64 @@ class GameLogic:
         self.ships_data = None
         self.plan = None
 
-        self.enemy_position_history = []
+        self.histories = {}
         self.turn = 0
+        self.last_shot = 0;
 
     def send_start(self):
-        return [50, self.resource - (50 * 1) - (20 * 12),  20, 1]
+        return [20, (self.resource - (20 * 1) - (20 * 12) - (1 * 2)) // 4,  20, 1]
 
     def recv_commands(self, data):
         if data[3] is not None:
             self.game_tick = data[3][0]
             self.ships_data = data[3][2]
 
-    def is_enemy_stopping(self):
-        return 10 < len(self.enemy_position_history) and reduce(lambda x, y: x and y, map(lambda x: x == self.enemy_position_history[-1], self.enemy_position_history))
-
-    def register_enemy_history(self, e):
-        self.enemy_position_history.append(e)
+    def distance(self, a, b):
+        x = (a[0] - b[0])
+        y = (a[1] - b[1])
+        return math.sqrt(x * x + y * y)
 
     def ship_distance(self, o, e):
-        x = (e.pos[0] - o.pos[0])
-        y = (e.pos[1] - o.pos[1])
-        return math.sqrt(x * x + y * y)
+        return self.distance(o.pos, e.pos)
+
+    def is_enemy_stopping(self, ehistory):
+        if len(ehistory) < 10:
+            return False
+        a = map(lambda x: self.ship_distance(x, ehistory[-1]) <= 5.0, ehistory[-10:])
+        return reduce(lambda x, y: x and y, list(a))
 
     def send_commands(self):
         my_ship_id = None
         my_p = None
         my_v = None
-        enemy_ship = None
         own_ship = None
         for (role, shipId, p, v, param, x5, x6, x7), appliedCommands in self.ships_data:
+            ship = Ship(role, shipId, p, v, param, x5, x6, x7)
+            if shipId not in self.histories:
+                self.histories[shipId] = []
+            self.histories[shipId].append(ship)
             if role == self.my_role:
                 my_ship_id = shipId
                 my_p = p
                 my_v = v
-                own_ship = Ship(role, shipId, p, v, param, x5, x6, x7)
-            else:
-                enemy_ship = Ship(role, shipId, p, v, param, x5, x6, x7)
-        res = []
+                own_ship = ship
 
-        self.register_enemy_history(enemy_ship)
-        if self.turn % 10 == 0 and self.ship_distance(own_ship, enemy_ship) < 50.0 and self.is_enemy_stopping():
-            res.append([2, enemy_ship.shipId, enemy_ship.pos, 100])
+        print("TURN:", self.turn)
+        print(self.turn - self.last_shot)
+        res = []
+        if 5 < self.turn - self.last_shot:
+            for key in self.histories:
+                if key == my_ship_id:
+                    continue
+                val = self.histories[key]
+                print(self.ship_distance(own_ship, val[-1]))
+                print(self.ship_distance(own_ship, val[-1]) < 50.0)
+                print(self.is_enemy_stopping(val))
+                if self.ship_distance(own_ship, val[-1]) < 50.0 and self.is_enemy_stopping(val):
+                    res.append([2, my_ship_id, val[0].pos, 70])
+                    self.last_shot = self.turn
+                    break
+        self.turn += 1
 
         if self.game_tick == 0:
             self.plan = calc_plan(my_p, my_v, self.max_turn, self.radius)
@@ -76,5 +93,4 @@ class GameLogic:
         if self.game_tick < len(self.plan):
             res.append([0, my_ship_id, self.plan[self.game_tick]])
 
-        self.turn += 1
         return res
