d49938bcd0d204b479ce09a6fc8527fb9665ebb9
hasipon <hasipon@users.noreply.github.com>
2020-07-19 19:41:18 +0000

    survive

--- app/hasi.py
+++ app/hasi.py
@@ -1,44 +1,90 @@
+import random
+from typing import *
+
+directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+
+
+def calc_orbit(p0: Tuple[int, int], v0: Tuple[int, int], aa: List[Tuple[int, int]], d: int):
+    p = p0
+    v = v0
+    orbit = [p0]
+    for i in range(d-1):
+        ax, ay = 0, 0
+        if abs(p[0]) >= abs(p[1]):
+            ax = -1 if p[0] > 0 else 1
+        if abs(p[0]) <= abs(p[1]):
+            ay = -1 if p[1] > 0 else 1
+        if i < len(aa):
+            ax -= aa[i][0]
+            ay -= aa[i][1]
+        v = (v[0] + ax, v[1] + ay)
+        p = (p[0] + v[0], p[1] + v[1])
+        orbit.append(p)
+    return orbit
+
+
+def calc_life(orbit: List[Tuple[int, int]], radius: int):
+    for i, p in enumerate(orbit):
+        if max(abs(p[0]), abs(p[1])) <= radius:
+            return i
+    return len(orbit)
+
+
+def calc_plan(my_p, my_v, max_turn, radius):
+    plan = None
+    life = -1
+    for l in range(1, 5):
+        for p in range(1 << (3 * l)):
+            a = [directions[(p >> (3 * i)) & 7] for i in range(l)]
+            orbit = calc_orbit(my_p, my_v, a, max_turn)
+            b = calc_life(orbit, radius)
+            if b == max_turn:
+                return a
+            if b > life:
+                plan = a
+                life = b
+    return plan
+
+
 class GameLogic:
     def __init__(self, static_game_info):
+        self.max_turn = static_game_info[0]
         self.my_role = static_game_info[1]
-        self.static_game_info = static_game_info
-        self.game_response = None
+        self.resource = static_game_info[2][0]
+        # unknown static_game_info[2][1]
+        # unknown static_game_info[2][2]
+        self.radius = None
+        self.safe_radius = None
+        if static_game_info[3] is not None:
+            self.radius = static_game_info[3][0]
+            self.safe_radius = static_game_info[3][1]
+        self.game_tick = None
+        self.ships_data = None
+        self.plan = None
 
     def send_start(self):
-        return [self.static_game_info[2][0] - 194, 0, 16, 1]
+        return [self.resource - 194, 0, 16, 1]
 
     def recv_commands(self, data):
-        self.game_response = data
+        if data[3] is not None:
+            self.game_tick = data[3][0]
+            self.ships_data = data[3][2]
 
     def send_commands(self):
         my_ship_id = None
-        tx, ty = 0, 0
-        ax, ay = 0, 0
-        fuel = None
-        for (role, shipId, (px, py), (vx, vy), x4, x5, x6, x7), appliedCommands in self.game_response[3][2]:
+        my_p = None
+        my_v = None
+        for (role, shipId, p, v, x4, x5, x6, x7), appliedCommands in self.ships_data:
             if role == self.my_role:
                 my_ship_id = shipId
-                fuel = x4[0]
-                print(f'p=({px},{py}) v=({vx},{vy})')
-                if abs(px) >= abs(py):
-                    ax = -1 if px > 0 else 1
-                if abs(px) <= abs(py):
-                    ay = -1 if py > 0 else 1
-                if vx + ax > 0:
-                    tx = 1
-                elif vx + ax < 0:
-                    tx = -1
-                if vy + ay > 0:
-                    ty = 1
-                elif vy + ay < 0:
-                    ty = -1
+                my_p = p
+                my_v = v
 
-        print(f'a=({ax},{ay})')
-        print(f't=({tx},{ty})')
-        print(f'fuel={fuel}')
+        if self.game_tick == 0:
+            self.plan = calc_plan(my_p, my_v, self.max_turn, self.radius)
+            print('plan:', self.plan)
 
-        print(f'my_ship_id = {my_ship_id}')
         res = []
-        if tx != 0 or ty != 0 and fuel > 0:
-            res.append([0, my_ship_id, (tx, ty)])
+        if self.game_tick < len(self.plan):
+            res.append([0, my_ship_id, self.plan[self.game_tick]])
         return res
